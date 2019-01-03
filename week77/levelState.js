7ef87e1e1c41130961807adca56f27c98e91bcd9
ztstroud <ztstroud@gmail.com>
2019-01-02 22:06:18 -0700

    Added basic gameplay

--- week77/levelState.js
+++ week77/levelState.js
@@ -3,40 +3,120 @@ var levelState = {
    
   ringIndex: 0,
   
-  x: 350,
-  y: 200,
-  z: 0,
+  entities: [{x: 350, y:200, z: 100, radius: 50, type:"player", color:"#00FF00", remove: false, dead: false}],
+  
+  blockSpawnTimer: 100,
+  blockSpawnTime: 100,
+  
+  coinSpawnTimer: 80,
+  coinSpawnTime: 80,
+  
+  score: 0,
     
   update: function() {
     this.ringIndex = (this.ringIndex + 1) % this.ringFrames.length;
     
-    if(InputManager.getKey(37)) {
-        this.x -= 3;
+    if(this.blockSpawnTimer > 0) {
+      this.blockSpawnTimer -= 1;
+    } else {
+      this.blockSpawnTimer = this.blockSpawnTime;
+      
+      if(this.blockSpawnTime > 20)
+        this.blockSpawnTime -= 2;
+      
+      let angle = Math.random() * 2 * Math.PI;
+      let entity = {
+        x: GraphicsManager.canvas.width / 2 + Math.cos(angle) * 71,
+        y: GraphicsManager.canvas.height / 2 + Math.sin(angle) * 71,
+        z: 0,
+        
+        type: "block",
+        color: "#FF0000",
+        
+        radius: 20 + 20 * Math.random()
+      };
+      
+      this.entities.push(entity)
     }
     
-    if(InputManager.getKey(39)) {
-        this.x += 3;
+    if(this.coinSpawnTimer > 0) {
+      this.coinSpawnTimer -= 1;
+    } else {
+      this.coinSpawnTimer = this.coinSpawnTime;
+      
+      if(this.coinSpawnTime > 40)
+          this.coinSpawnTime -= 1;
+      
+      let angle = Math.random() * 2 * Math.PI;
+      let entity = {
+        x: GraphicsManager.canvas.width / 2 + Math.cos(angle) * 71,
+        y: GraphicsManager.canvas.height / 2 + Math.sin(angle) * 71,
+        z: 0,
+        
+        type: "coin",
+        color: "#FFFF00",
+        
+        radius: 20 + 20 * Math.random()
+      };
+      
+      this.entities.push(entity)
     }
     
-    if(InputManager.getKey(38)) {
-        this.y -= 3;
+    if(this.entities.length > 0 && this.entities[0].type === "player" && !this.entities[0].dead) {
+      let player = this.entities[0];
+      
+      if(InputManager.getKey(37)) {
+        player.x -= 3;
+      }
+      
+      if(InputManager.getKey(39)) {
+        player.x += 3;
+      }
+      
+      if(InputManager.getKey(38)) {
+        player.y -= 3;
+      }
+      
+      if(InputManager.getKey(40)) {
+        player.y += 3;
+      }
+      
+      
+      let playerRadius = player.radius * (player.z / 255.0)
+      for(let index = 1; index < this.entities.length; index++) {
+        let entity = this.entities[index];
+        let entityRadius = entity.radius * (entity.z / 255.);
+        
+        let dx = player.x - entity.x;
+        let dy = player.y - entity.y;
+        
+        let dist = Math.sqrt(dx * dx + dy * dy);
+        if(dist < playerRadius + entityRadius) {
+          if(entity.type === "coin") {
+            this.score += 1;
+            
+            entity.remove = true;
+          } else if(entity.type === "block") {
+            player.dead = true;
+          }
+        }
+      }
     }
     
-    if(InputManager.getKey(40)) {
-        this.y += 3;
-    }
-    
-    let dx = this.x - GraphicsManager.canvas.width / 2;
-    let dy = this.y - GraphicsManager.canvas.height / 2;
-    
-    let distance = Math.sqrt(dx * dx + dy * dy);
-    
-    if(distance <= 225 && distance >= 70) {
+    for(var index in this.entities) {
+      let entity = this.entities[index];
+      
+      let dx = entity.x - GraphicsManager.canvas.width / 2;
+      let dy = entity.y - GraphicsManager.canvas.height / 2;
+      
+      let distance = Math.sqrt(dx * dx + dy * dy);
+      
+      if(distance <= 225 && distance >= 70) {  
         let vx = dx / distance;
         let vy = dy / distance;
         
-        this.x += vx * 0.55;
-        this.y += vy * 0.55;
+        entity.x += vx * 0.55;
+        entity.y += vy * 0.55;
         
         
         let offset = Math.abs((distance - 70) - 77.5) / 77.5;
@@ -44,12 +124,27 @@ var levelState = {
         offset *= offset;
         offset = 1.0 - offset;
         
-        this.z = 100 + offset * 77.5;
-    } else if(this.z > 0) {
-        this.z -= 1;
+        entity.z = 50 + offset * 77.5;
+      } else if(entity.z > 0) {
+        if(entity.type === "player")
+          entity.dead = true;
+         
+        entity.z -= 1;
         
-        if(this.z < 0)
-            this.z = 0;
+        if(entity.z < 0)
+          entity.z = 0;
+      } else {
+        entity.remove = true;
+      }
+    }
+    
+    let removeIndex = 0;
+    while(removeIndex < this.entities.length) {
+      if(this.entities[removeIndex].remove) {
+        this.entities.splice(removeIndex, 1);
+      } else {
+        removeIndex += 1;
+      }
     }
   },
   
@@ -57,9 +152,15 @@ var levelState = {
     ctx.clearRect(0, 0, GraphicsManager.canvas.width, GraphicsManager.canvas.height);
     ctx.drawImage(ImageManager.images[this.ringFrames[this.ringIndex]], 235, 35);
     
-    let scale = (this.z / 255.0);
+    for(var index in this.entities) {
+      let scale = (this.entities[index].z / 255.0);
     
-    this.drawCircle(ctx, this.x, this.y, 40 * scale, "#00FF00");
+      this.drawCircle(ctx, this.entities[index].x, this.entities[index].y, this.entities[index].radius * scale, this.entities[index].color);
+    }
+    
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "30px Arial";
+    ctx.fillText("Score: " + this.score, 10, 35);
   },
   
   drawCircle: function(ctx, x, y, r, color) {
