543c46db3dbe6d9f153fcd38f9a1943d3b4e852d
ztstroud <ztstroud@gmail.com>
2019-01-08 22:00:00 -0700

    Added week 78

---
+++ week78/engine.js
@@ -0,0 +1,134 @@
+var ImageManager = {
+  images: {},
+  registeredImages: [],
+  
+  loadedCount: 0,
+  totalCount: 0,
+  
+  allMediaLoaded: undefined,
+  
+  registerImages: function(imagePaths) {
+    ImageManager.registeredImages.push(...imagePaths);
+    ImageManager.totalCount += imagePaths.length;
+  },
+  
+  loadImages: function() {
+    while(ImageManager.registeredImages.length > 0) {
+      let imagePath = ImageManager.registeredImages.shift();
+      
+      var image = new Image();
+      image.onload = ImageManager.imageLoaded;
+      
+      image.src = imagePath;
+      
+      ImageManager.images[imagePath] = image;
+    }
+  },
+  
+  imageLoaded: function() {
+    ImageManager.loadedCount += 1;
+    
+    if(ImageManager.loadedCount === ImageManager.totalCount && ImageManager.allMediaLoaded !== undefined) {
+      ImageManager.allMediaLoaded();
+    }
+  }
+}
+
+var InputManager = {
+    keys: {},
+    
+    mouseX: 0,
+    mouseY: 0,
+    
+    mousePresent: false,
+    
+    owner: undefined,
+    
+    bind: function(element) {
+        InputManager.owner = element;
+        
+        document.body.addEventListener("keydown", InputManager.keyDown);
+        document.body.addEventListener("keyup", InputManager.keyUp);
+        
+        element.addEventListener("mouseenter", InputManager.setMousePosition);
+        element.addEventListener("mousemove", InputManager.setMousePosition);
+        element.addEventListener("mouseleave", InputManager.mouseLeave);
+    },
+    
+    keyDown: function(event) {
+        InputManager.keys[event.keyCode] = true;
+        event.preventDefault();
+    },
+    
+    keyUp: function(event) {
+        InputManager.keys[event.keyCode] = false;
+        event.preventDefault();
+    },
+    
+    getKey: function(keyCode) {
+        return InputManager.keys.hasOwnProperty(keyCode) && InputManager.keys[keyCode];
+    },
+    
+    setMousePosition: function(event) {
+        let rect = InputManager.owner.getBoundingClientRect();
+        
+        InputManager.mouseX = event.clientX - rect.left;
+        InputManager.mouseY = event.clientY - rect.top;
+        
+        InputManager.mousePresent = true;
+    },
+    
+    mouseLeave: function(event) {
+        InputManager.mousePresent = false;
+    }
+}
+
+var GraphicsManager = {
+  canvas: undefined,
+  ctx: undefined,
+    
+  setCanvas: function(canvas) {
+    GraphicsManager.canvas = canvas;
+    GraphicsManager.ctx = canvas.getContext("2d");
+  }
+}
+
+var StateManager = {
+  states: [],
+  
+  popState: function() {
+    if(StateManager.states.length > 0)
+      StateManager.states.pop();
+  },
+  
+  pushState: function(state) {
+    StateManager.states.push(state);
+  },
+  
+  setState: function(state) {
+    StateManager.popState();
+    StateManager.pushState(state);
+  },
+  
+  tick: function() {
+    if(StateManager.states.length === 0)
+      return;
+      
+    let state = StateManager.states[StateManager.states.length - 1];
+    
+    state.update();
+    state.draw(GraphicsManager.ctx);
+  }
+}
+
+var Engine = {
+  gameLoop: function() {
+    StateManager.tick();
+    
+    requestAnimationFrame(Engine.gameLoop);
+  },
+  
+  start: function() {
+    requestAnimationFrame(Engine.gameLoop);
+  }
+}
